f7dee092021b682971a57a66a7847d59ae332e99
Ly <38577181+lilypham123@users.noreply.github.com>
2022-12-30 14:01:52 +0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,34 @@
+﻿// Nhập bán kính. Viết hàm trả về 2 giá trị chu vi và diện tích hình tròn
+
+#include <stdio.h>
+#include <conio.h>
+#define pi 3.14
+
+typedef struct TT
+{
+	float chuvi;
+	float dientich;
+};
+
+TT tinhtoan(int n)
+{
+	TT A;
+	A.chuvi = 2*pi*n;
+	A.dientich = pi*n*n;
+	return A;
+}
+
+int main()
+{
+	int bk;
+	
+	printf("Nhap vao ban kinh: ");
+	scanf("%d", &bk);
+
+	TT A = tinhtoan(bk);
+	printf("Chu vi hinh tron la: C = %f", A.chuvi);
+	printf("\nDien tich hinh tron la: S = %f", A.dientich);
+
+	getch();
+	return 0;
+}
