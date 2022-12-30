868bff38d8f595bf61b09a64260a287815385396
Ly <38577181+lilypham123@users.noreply.github.com>
2022-12-30 14:00:53 +0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#define CLEARBIT(byte, pos)
+
+// Viết chương trình clear 1 bit ở vị trí bất kỳ của biến 32 bit
+
+int main()
+{
+    int byte;
+	printf("Nhap vao 1 so 32bit: ");
+	scanf("%d", &byte);
+    int pos;
+    printf("Nhap pos: ");
+    scanf("%d", &pos);
+    byte &= ~(1<<pos);
+    printf("So sau set bit la: %d", byte);
+	getch();
+    return 0;
+}
